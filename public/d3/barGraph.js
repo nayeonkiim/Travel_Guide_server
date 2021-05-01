91cc08e31a5a833e3f48e10f35c5e0e2357fec7e
nayeonkiim <wkdrnsldk98@naver.com>
2021-05-01 19:02:59 +0900

    막대그래프

---
+++ public/d3/barGraph.js
@@ -0,0 +1,100 @@
+
+var dataArr = [ // <-A
+    { count: 0 },
+    { count: 0 },
+    { count: 0 },
+    { count: 0 },
+    { count: 0 },
+    { count: 0 }
+];
+
+function select(category) {
+    const fordata = { 'subPlace': category };
+    axios.post('/map/ages', fordata)
+        .then((res) => {
+            console.log("dataArr: " + dataArr);
+            for (let i = 0; i < res.data.length; i++) {
+                console.log(res.data[i].age);
+                if (res.data[i].age >= 20 && res.data[i].age < 30) {
+                    dataArr[0].count += 1;
+                    dataArr[0].category = "20대";
+                } else if (res.data[i].age >= 30 && res.data[i].age < 40) {
+                    dataArr[1].count += 1;
+                    dataArr[1].category = "30대"
+                } else if (res.data[i].age >= 40 && res.data[i].age < 50) {
+                    dataArr[2].count += 1;
+                    dataArr[2].category = "40대";
+                } else if (res.data[i].age >= 50 && res.data[i].age < 60) {
+                    dataArr[3].count += 1;
+                    dataArr[3].category = "50대";
+                } else if (res.data[i].age >= 60 && res.data[i].age < 70) {
+                    dataArr[4].count += 1;
+                    dataArr[4].category = "60대";
+                } else {
+                    dataArr[5].count += 1;
+                    dataArr[5].category = "70대이상";
+                }
+            }
+        }).then((res) => {
+            console.log(dataArr);
+            render(dataArr);
+        }).then((res) => {
+            for (let k = 0; k < dataArr.length; k++) {
+                dataArr[k].count = 0;
+                dataArr[k].category = "";
+            }
+        })
+}
+
+var width = 500;
+var height = 400;
+
+var color = d3.scale
+    .linear()
+    .domain([0, 60])
+    .range(["red", "blue"]);
+
+var witdhScale = d3.scale
+    .linear()
+    .domain([0, 60])
+    .range([0, width - 20]);
+
+var axis = d3.svg.axis().scale(witdhScale);
+
+var canvas = d3.select("#container").append("svg").attr("width", width).attr("height", height).append("g").attr("transform", "translate(10, 20)");
+
+function render(dataArr) {
+    canvas.selectAll("rect")
+        .data(dataArr)
+        .enter()
+        .append("rect")
+
+    canvas.selectAll("rect")
+        .data(dataArr)
+        .exit().remove();
+
+    canvas.selectAll("rect")
+        .attr("width", function (d) {
+            return witdhScale(d.count);
+        })
+        .attr('height', 50)
+        .attr("fill", function (d) {
+            return color(d.count);
+        })
+        .attr('y', function (d, i) {
+            return i * 60
+        })
+
+    canvas.data(dataArr)
+        .append("text")
+
+    canvas.selectAll("text")
+        .data(dataArr)
+        .exit().remove()
+
+    canvas.selectAll("text")
+        .attr("dy", ".35em")
+        .text(function (d) { return d.category });
+
+    canvas.append("g").attr("transform", "translate(0, 300)").call(axis);
+}
