b47b8d961df00a2a6cd410595537569e07f9f24c
nayeonkiim <wkdrnsldk98@naver.com>
2021-05-09 14:34:11 +0900

    여행 상품 등록 라우터 수정

--- routes/route.js
+++ routes/route.js
@@ -3,37 +3,51 @@ const Route = require('../models/route');
 const Product = require('../models/product');
 const router = express.Router();
 const { sequelize } = require('../models');
+const TourPlace = require('../models/tourplace');
 
 router.post('/', async (req, res, next) => {
     console.log("여행 상품 등록 라우터 호출");
     console.log(req.body);
     const title = req.body.title;
-    const route = req.body.route;
+    const schedule = req.body.schedule;
     const introduce = req.body.introduce;
     const memo = req.body.memo;
 
     try {
-        //먼저 schedule에 해당하는 정보 routes 테이블에 저장
+        //상품 등록
         const result = await sequelize.transaction(async (t) => {
-            const addProduct = await Product.create({
-                title,
-                introduce,
-                memo
-            }, { transaction: t });
-
-            for (var i = 1; i <= route.lenght; i++) {
-                var arr = route.i;
-                for (var j = 0; j < arr.lenght; j++) {
+            for (var i = 1; i <= Object.keys(schedule).length; i++) {
+                //숫자 parsing
+                const keyy = i.toString();
+                var arr = schedule[keyy];
+                console.log(arr);
+                for (var j = 0; j < Object.keys(arr).length; j++) {
+                    //일정 경로 route 등록
                     const addRoute = await Route.create({
                         name: arr[j].name,
                         startTime: arr[j].startTime,
                         endTime: arr[j].endTime,
                         freeTime: arr[j].freeTimeChk,
                         day: arr[j].day,
-                    }, { transaction: t }
+                    })
                         .then(async addRoute => {
+                            //상품 등록
+                            const addProduct = await Product.create({
+                                title,
+                                introduce,
+                                memo
+                            }, { transaction: t });
+
+                            console.log("addRouter: " + addRoute);
+                            //장소 조회
+                            const tourPlace = await TourPlace.findOne({
+                                where: { name: addRoute.name }
+                            });
+
+                            //장소와 상품에 연관관계 맺어주기
+                            await addRoute.setTourPlace(tourPlace, { transaction: t })
                             await addRoute.setProduct(addProduct, { transaction: t });
-                        }));
+                        });
                 }
             }
             return true;
